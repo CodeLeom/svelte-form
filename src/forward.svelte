2bc3065700b4273455381562f1fa95243281b4e2
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2022-08-18 17:45:46 +0100

    added shorthand code for a forwarding event in svelte

--- src/forward.svelte
+++ src/forward.svelte
@@ -1,3 +1,4 @@
+
 <script>
 	import Event from './event.svelte';
 	import { createEventDispatcher } from 'svelte';
@@ -10,3 +11,19 @@
 </script>
 
 <Event on:message={forward}/>
+
+
+
+<!-- 
+    below is a short hand for the code above. which will still works normal as the code above will do 
+
+    you can comment out the code above and uncomment the code below, it will still work perfectly. This explain the event forwarding in svelte js.
+-->
+
+
+<!-- 
+<script>
+	import Event from './Event.svelte';
+</script>
+
+<Event on:message/> -->
